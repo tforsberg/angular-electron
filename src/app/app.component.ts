d9931a861198f90b4a6e3c91991aa47dea7bda19
Todd Forsberg <tforsberg@gmail.com>
2018-12-25 21:42:41 -0600

    broken - formControl not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { ElectronService } from './providers/electron.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 import { TranslateService } from '@ngx-translate/core';
+
+import { filter, switchMap, debounceTime, catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
+
+
+import { ElectronService } from './providers/electron.service';
 import { AppConfig } from '../environments/environment';
+import { User } from './models/user.model';
+import { GithubService } from './services/github.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(public electronService: ElectronService,
-    private translate: TranslateService) {
+export class AppComponent implements OnInit {
+
+  findControl = new FormControl();
+
+  error = false;
+
+  user: User = null;
+
+
+  constructor(
+      public electronService: ElectronService,
+      private translate: TranslateService,
+      private githubService: GithubService) {
 
     translate.setDefaultLang('en');
     console.log('AppConfig', AppConfig);
@@ -23,4 +43,31 @@ export class AppComponent {
       console.log('Mode web');
     }
   }
+
+  ngOnInit() {
+    this.findControl.valueChanges
+      .pipe(
+        // Фильтруем если введено меньше двух символов
+        filter(value => value.length > 2),
+        // Ставим задержку одну секунду
+        debounceTime(1000),
+        // Запрашиваем данные пользователя
+        switchMap(value =>
+          this.githubService.getUser(value).pipe(
+            // Обработка ошибок
+            catchError(err => {
+              this.user = null;
+              this.error = true;
+              return EMPTY;
+            })
+          )
+        )
+      )
+      // Получение данных
+      .subscribe(user => {
+        this.user = user;
+        this.error = false;
+      });
+  }
+
 }
